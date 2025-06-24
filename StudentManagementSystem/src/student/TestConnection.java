a494b9c9913d3f62adb5916ac419c4fd438d7f7b
Sangeetha-A <sangeethaashok212002@gmail.com>
2025-06-24 11:50:33 +0530

    Update TestConnection.java

--- StudentManagementSystem/src/student/TestConnection.java
+++ StudentManagementSystem/src/student/TestConnection.java
@@ -4,9 +4,9 @@ import java.sql.SQLException;
 public class TestConnection {
 	public static void main(String[] args) {
         try (Connection conn = DBConnection.getConnection()) {
-            System.out.println("✅ Connection successful!");
+            System.out.println("Connection successful!");
         } catch (SQLException e) {
-            System.out.println("❌ Connection failed!");
+            System.out.println("Connection failed!");
             e.printStackTrace();
         }
     }
